b05fc0f391c2820384b6aea6fc3af3f2303e4fcd
Fernanda Diomede <34989708+fcdiomede@users.noreply.github.com>
2020-06-11 22:21:48 -0500

    added in collision logic

--- galaxy_racers.py
+++ galaxy_racers.py
@@ -14,12 +14,33 @@ class spaceship(object):
 		self.dir_index = 0
 		self.smoke_trail = []
 		self.smoke_count = 0
+		self.hitbox = (self.x + 17, self.y + 2, 31, 57)
 
 	def draw(self,win):
 		self.move(win)
+		for rectangle in self.smoke_trail:
+			win.blit(cloud_img, (rectangle[0],rectangle[1]))
 		win.blit(spaceship_img[self.dir_index], (self.x, self.y))
-		for pos in self.smoke_trail:
-			win.blit(cloud_img, pos)
+
+		self.hitbox_pos()
+		#pygame.draw.rect(win, (255,0,0), self.hitbox,2)
+		#commented out code that makes hitbox visible
+		self.collision()
+
+	def hitbox_pos(self):
+		#adjusts hitbox positioning depending on the spaceship direction
+		#if moving left
+		if self.dirx == -1:
+			self.hitbox = (self.x + 13, self.y + 6, 60, 37)
+		#if moving right
+		elif self.dirx == 1:
+			self.hitbox = (self.x + 23, self.y + 6 ,60, 37)
+		#if moving up
+		elif self.diry == -1:
+			self.hitbox = (self.x + 6, self.y + 13,37, 60)
+		#else, I must be moving down
+		else:
+			self.hitbox = (self.x + 6, self.y + 23 , 37, 60)
 		
 
 	def move(self,win):
@@ -43,14 +64,34 @@ class spaceship(object):
 				self.diry = 1
 				self.dir_index = 3
 
+		#adds in smoke cloud positions to a list
 		self.smoke_count += 1
 		if self.smoke_count % 7 == 0:
-			self.smoke_trail.append((self.x,self.y))
+			self.smoke_trail.append(pygame.Rect(self.x, self.y,30,30))
+
 		self.x += self.vel * self.dirx
 		self.y += self.vel * self.diry
 
 	def collision(self):
-		pass
+		#if moving left
+		if self.dirx == -1 and self.hitbox[0] <= 0:
+			return True
+		#if moving right
+		elif self.dirx == 1 and self.hitbox[0] >= 980:
+			return True
+		#if moving up
+		elif self.diry == -1 and self.hitbox[1] <= 0:
+			return True
+		#else, I must be moving down
+		elif self.diry == 1 and self.hitbox[1] >= 780:
+			return True
+		
+		
+		if len(self.smoke_trail) > 5:
+			wall = self.smoke_trail[:len(self.smoke_trail)-5]
+			spaceship_rect = pygame.Rect(self.hitbox)
+			if spaceship_rect.collidelist(wall) > -1:
+				return True
 
 def redrawGameWindow(win):
 	global player
